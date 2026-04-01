f4044d87be54209c613b5c6503c55c699f19d97d
Ajimi Mahdi <ajimimahdi1@gmail.com>
2026-04-01 21:24:26 +0200

    inserimento i commenti

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+// Importa il router della bacheca
+const bachecaRouter = require('./routers/bacheca');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('server del blog');
 });
+// Usa il router della bacheca per tutte le rotte che iniziano con /bacheca
+app.use('/bacheca', bachecaRouter);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- routers/bacheca.js
+++ routers/bacheca.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const router= express.Router();
+// Importa il controller della bacheca
 const bachecaController = require('../controllers/bachecaController');
+
+// Definisci le rotte per la bacheca
 
 router.get('/', bachecaController.index);
 router.get('/:id', bachecaController.show);
@@ -9,5 +12,6 @@ router.put('/:id', bachecaController.update);
 router.patch('/:id', bachecaController.modify);
 router.delete('/:id', bachecaController.destroy);
 
+// Esporta il router
 
 module.exports = router;
